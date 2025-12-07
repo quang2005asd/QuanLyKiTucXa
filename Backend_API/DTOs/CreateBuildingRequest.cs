2038156ea2ec9d73c0c47003f2b5a379d2604a96
Nguyễn Việt Quang <quangcokhi2005@gmail.com>
2025-12-07 19:33:56 +0700

    Delete Backend_API directory

--- Backend_API/DTOs/CreateBuildingRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace Backend_API.DTOs
-{
-    public class CreateBuildingRequest
-    {
-        public string Name { get; set; } = string.Empty; 
-        public int TotalFloors { get; set; }          
-        public int RoomsPerFloor { get; set; }          
-    }
-}
--- Backend_API/Models/Building.cs
+++
@@ -1,9 +0,0 @@
-namespace Backend_API.Models
-{
-    public class Building
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty; // Ví dụ: Nhà A1
-        public int TotalFloors { get; set; }
-    }
-}
